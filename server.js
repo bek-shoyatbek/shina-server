361d48e180ada1014778282a35b866c6619e0ab4
bek <shoyatbekov03032003@gmail.com>
2023-11-02 06:13:26 +0000

    with ssl sertifications

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 import express from "express"
 import cors from "cors"
+import {fileURLToPath } from "url";
+
 
 import { config } from "dotenv";
 
 import { bot } from "./index.js"
+import path from "path";
 
 
 const app = express();
 config();
 
+const __filename = fileURLToPath(import.meta.url);
+
+
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname,"build")));
 app.use(cors())
+
 
 app.set('view engine', 'ejs');
 
@@ -23,6 +33,9 @@ import { router } from "./routes.js";
 
 app.use("/api", router);
 
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"build","index.html"));
+})
 
 
 const port = process.env.PORT || 3333;
